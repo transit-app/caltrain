10dd361cf4ab9508e48e0948dc8d4a9ecfcd4430
bschuetzle <bill_schuetzle@sbcglobal.net>
2017-07-14 11:16:11 -0700

    initial seeds file for stations, trains, and stops

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,57 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+require 'csv'
+
+# delete / destroy all the data first
+Station.destroy_all
+Train.destroy_all
+Stop.destroy_all
+puts "=> All data deleted"
+
+
+# reset the primary keys to start with id=1
+ActiveRecord::Base.connection.reset_pk_sequence!('stations')
+ActiveRecord::Base.connection.reset_pk_sequence!('trains')
+ActiveRecord::Base.connection.reset_pk_sequence!('stops')
+
+
+# load Stations data from csv file in lib/seeds
+puts "=> Starting load of Stations data"
+csv_text = File.read(Rails.root.join('lib', 'seeds', 'stations_data.csv'))
+csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1')
+csv.each do |row|
+  s = Station.new
+  s.name = row['name']
+  s.save
+  puts "   Station #{s.name} saved"
+end
+puts "=> There are now #{Station.count} rows in the stations table"
+
+
+# load Trains data from csv file in lib/seeds
+puts "=> Starting load of Trains data"
+csv_text = File.read(Rails.root.join('lib', 'seeds', 'trains_data.csv'))
+csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1')
+csv.each do |row|
+  t = Train.new
+  t.train_number = row['train_number']
+  t.direction = row['direction']
+  t.service_type = row['service_type']
+  t.save
+  puts "   Train Number #{t.train_number} saved"
+end
+puts "=> There are now #{Train.count} rows in the trains table"
+
+
+# load Stops data from csv file in lib/seeds
+puts "=> Starting load of Stops data"
+csv_text = File.read(Rails.root.join('lib', 'seeds', 'stops_data.csv'))
+csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1')
+csv.each do |row|
+  s = Stop.new
+  s.station_id = row['station_id']
+  s.train_id = row['train_id']
+  s.departure_time = row['departure_time']
+  s.save
+  puts "   Stop with station_id #{s.station_id}, train_id #{s.train_id} saved"
+end
+puts "=> There are now #{Stop.count} rows in the stops table"
