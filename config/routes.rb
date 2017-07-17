f02ac614ab41df63d9506a504fd5e080e3694bfc
kabitachatterjee <chatterjeekavita01@gmail.com>
2017-07-16 19:17:04 -0700

    added root route to our application with user login

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
   get "/sign_in" => "clearance/sessions#new", as: "sign_in"
   delete "/sign_out" => "clearance/sessions#destroy", as: "sign_out"
   get "/sign_up" => "clearance/users#new", as: "sign_up"
-  get '/stations', to: 'stations#index', as: 'stations'
+  get '/', to: 'stations#index', as: 'stations'
   get '/stops', to: 'stops#index', as: 'stops'
   post '/stations', to: 'stops#index'
   get '/stops/:id', to: 'stops#show', as:'stops_details'
