61ee4af334d97777fe87a3ae0950eb25db5f4688
kabitachatterjee <chatterjeekavita01@gmail.com>
2017-07-17 15:21:14 -0700

    added columns for source and destination for favorite model

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,24 +2,26 @@ class FavoritesController < ApplicationController
   def index
     user_Id = current_user.id
     @favorites = Favorite.where(user_id:user_Id)
+
   end
   # def create
   #   redirect_to '/favorites'
   # end
 
   def create
-    stop_id = params[:stop_id].to_i
+    #stop_id = params[:stop_id].to_i
+    from = params[:from]
+    to = params[:to]
     user_id = params[:user_id].to_i
-    @favorite = current_user.favorites.build({stop_id:stop_id, user_id:user_id})
+    @favorite = current_user.favorites.build({user_id:user_id, source:from, destination:to})
 
       if @favorite.valid?
         @favorite.save
         redirect_to favorites_path
       else
         @favorite = Favorite.create
-        flash[:error] = "Oops!" + @favorite.errors.messages[:title][0] + "."
+        flash[:error] = "Oops!"
         redirect_to '/'
-        explode
     end
   end
   private
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170717174138) do
+ActiveRecord::Schema.define(version: 20170717220823) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,8 @@ ActiveRecord::Schema.define(version: 20170717174138) do
     t.bigint "stop_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "source"
+    t.string "destination"
     t.index ["stop_id"], name: "index_favorites_on_stop_id"
     t.index ["user_id"], name: "index_favorites_on_user_id"
   end
