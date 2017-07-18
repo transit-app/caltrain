d18d21cbcbe9fa7d2f739afae1ceb2123791f923
kabitachatterjee <chatterjeekavita01@gmail.com>
2017-07-18 09:04:07 -0700

    added handling the favorites dropdown when no user is logged in

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -4,21 +4,15 @@ class FavoritesController < ApplicationController
     @favorites = Favorite.where(user_id:user_Id)
 
   end
-  # def create
-  #   redirect_to '/favorites'
-  # end
 
   def create
-    #stop_id = params[:stop_id].to_i
     from = params[:from]
     to = params[:to]
     user_id = params[:user_id].to_i
     @favorite = current_user.favorites.build({user_id:user_id, source:from, destination:to})
 
       if @favorite.valid?
-
-        @favorite.save
-        redirect_to favorites_path
+          @favorite.save
 
       else
         @favorite = Favorite.create
--- app/controllers/stations_controller.rb
+++ app/controllers/stations_controller.rb
@@ -2,8 +2,10 @@ class StationsController < ApplicationController
   def index
     @station = Station.new
     @stations = Station.all
+    if current_user
     user_Id = current_user.id
       @favorites = Favorite.where(user_id:user_Id)
+    end
 
   end
 end
