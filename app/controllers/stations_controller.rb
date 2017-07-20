5b01780e9ff8cf69859cf0048c73de36a37d77d9
nathanallen <thenathanator@gmail.com>
2017-07-20 12:44:44 -0700

    remove @favorites from controllers, use association to retrieve

--- app/controllers/stations_controller.rb
+++ app/controllers/stations_controller.rb
@@ -1,10 +1,6 @@
 class StationsController < ApplicationController
   def index
     @stations = Station.all
-    if current_user
-      user_id = current_user.id
-      @favorites = Favorite.where(user_id:user_id)
-    end
   end
 
 end
--- app/controllers/stops_controller.rb
+++ app/controllers/stops_controller.rb
@@ -4,12 +4,7 @@ class StopsController < ApplicationController
     departure_station = Station.find(params[:from])
     arrival_station = Station.find(params[:to])
 
-    @trip_data = Station.get_trips(departure_station, arrival_station, params[:time], params[:range])
-
-    if current_user
-      user_id = current_user.id
-      @favorites = Favorite.where(user_id:user_id)
-    end
+    @trip_data = Trip.get_trips(departure_station, arrival_station, params[:time], params[:range])
   end
 
 
